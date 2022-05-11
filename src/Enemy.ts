1be397ac1ddf2622dde0edc2b4d0ad498a9be910
Mo21ON <voelknermoritz@outlook.de>
2022-05-11 11:20:53 +0200

    rename

--- src/Enemy.ts
+++ src/Enemy.ts
@@ -1,3 +1,9 @@
 import {Entity} from "./Entity";
 
-export class Enemy extends Entity{}
+export class Enemy extends Entity{
+    toDelete: boolean = false
+
+    constructor() {
+        super(1000, Math.random() < 0.5 ? 600 : 700, -5, 0, 100, 100);
+    }
+}
