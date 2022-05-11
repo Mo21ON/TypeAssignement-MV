6e28156defb5f9abc74aa003cd914cdf6434fd44
Mo21ON <voelknermoritz@outlook.de>
2022-05-11 10:58:34 +0200

    first variables

--- src/Game.ts
+++ src/Game.ts
@@ -1,2 +1,8 @@
 import {Enemy} from "./Enemy";
 import {Player} from "./Player";
+
+let canvas = document.getElementById('myCanvas') as HTMLCanvasElement
+let ctx = canvas.getContext('2d');
+
+let enemies: Enemy[] = [];
+let player = new Player();
