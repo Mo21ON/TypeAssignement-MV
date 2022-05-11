7f4036ce962f5d14abc645919d89231c7bec5a18
Mo21ON <voelknermoritz@outlook.de>
2022-05-11 11:21:52 +0200

    rename- module

--- src/Game.ts
+++
@@ -1,8 +0,0 @@
-import {Enemy} from "./Enemy";
-import {Player} from "./Player";
-
-let canvas = document.getElementById('myCanvas') as HTMLCanvasElement
-let ctx = canvas.getContext('2d');
-
-let enemies: Enemy[] = [];
-let player = new Player();
